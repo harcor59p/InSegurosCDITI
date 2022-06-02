b6a6b934f6ce34cddc866552734c7acddaf0c6f8
Harold Cortes <harcor59p@gmail.com>
2022-06-01 19:22:45 -0500

    Creacion de links a register y login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,21 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>In-Seguros-CDITI</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/welcome.css">
 </head>
 <body>
     <div class="container">
         <br>
         <h1 style="text-align:center">In-Seguros-CDITI</h1>
         <br><br>
-        <div align="center"><img src="logo.png" alt=""></div>
+        <div align="center"><img src="Logo.png" alt="No disponible"></div>
         <br><br>
-        <form action="" method="post">
-            <button type="button" class="btn btn-dark btn-lg btn-block">Registrarse</button>
-        </form>
+        <a href="register">
+            <button type="button" class="btn btn-secondary btn-lg btn-block">Registrarse</button>
+        </a>
         <br>
-        <form action="" method="post">
-            <button type="button" class="btn btn-dark btn-lg btn-block">Ingresar</button>
-        </form>
+        <a href="auth/login">
+            <button type="button" class="btn btn-secondary btn-lg btn-block">Ingresar</button>
+        </a>
     </div>
 </body>
 </html>
